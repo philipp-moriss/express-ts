a7e3cb317a805ff3aa973fd93aa4a59c91fbc936
philipp <philipp>
2022-06-03 11:15:48 +0300

    add class logger and dependenci ajection

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import {usersRouter} from './users/userRouter.js'
 import {Server} from 'http'
+import {LoggerService} from "./logger/logger.service";
+
 
 export class App {
     app : Express;
     port : number;
     server : Server;
+    logger : LoggerService
 
-    constructor() {
+    constructor(logger : LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger
     };
 
     useRoutes () {
@@ -19,6 +23,6 @@ export class App {
     public async init () {
         this.useRoutes()
         this.server = this.app.listen(this.port)
-        console.log(`server start on port : ${this.port}`)
+        this.logger.log(`server start on http://localhost:${this.port}`)
     };
 }
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,29 @@
+import  {Logger} from 'tslog'
+
+
+export class LoggerService {
+
+    private logger : Logger;
+
+    constructor () {
+        this.logger = new Logger({
+            displayInstanceName : false,
+            displayLoggerName : false,
+            displayFilePath : 'hidden',
+            displayFunctionName : false,
+        })
+    };
+
+    log (...args : unknown[]) {
+        this.logger.info(...args)
+    };
+
+    error (...args : unknown[]) {
+        this.logger.error(...args)
+    };
+
+    warning (...args : unknown[]) {
+        this.logger.warn(...args)
+    }
+
+}
