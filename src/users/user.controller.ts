d8ea396e08bd567c0757b12734ecea56c2f013e7
philipp-moriss <filverem@gmail.com>
2022-06-14 22:38:31 +0300

    add entity

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -8,6 +8,7 @@ import 'reflect-metadata';
 import { IUserController } from './user.interface';
 import { UserLoginDto } from '../dto/user-login.dto';
 import { UserRegisterDto } from '../dto/user-register.dto';
+import { User } from './user.entity';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -33,7 +34,13 @@ export class UserController extends BaseController implements IUserController {
 		//next(new HttpError(401, 'no autarization', 'login'));
 	}
 
-	register(req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void {
-		this.ok<string>(res, 'register');
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const newUser = new User(body.email, body.name);
+		await newUser.setPassword(body.password);
+		this.ok<User>(res, newUser);
 	}
 }
